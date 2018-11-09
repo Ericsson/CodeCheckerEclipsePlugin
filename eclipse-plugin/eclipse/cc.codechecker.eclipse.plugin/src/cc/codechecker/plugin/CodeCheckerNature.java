6cdc5dfdce19dee570e2a100455aa7842ce314b2
Vodorok <gabor.kutas@ericsson.com>
2018-11-09 23:37:47 +0100

    Added project related fields to CodeCheckerNature

--- eclipse-plugin/eclipse/cc.codechecker.eclipse.plugin/src/cc/codechecker/plugin/CodeCheckerNature.java
+++ eclipse-plugin/eclipse/cc.codechecker.eclipse.plugin/src/cc/codechecker/plugin/CodeCheckerNature.java
@@ -4,14 +4,17 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 
+import cc.codechecker.plugin.config.CodeCheckerContext;
+import cc.codechecker.plugin.runtime.CodecheckerServerThread;
+
 public class CodeCheckerNature implements IProjectNature {
 
     public static final String NATURE_ID = "cc.codechecker.plugin.CodeCheckerNature";
+    IProject project; 
 
     @Override
     public void configure() throws CoreException {
-        // TODO Auto-generated method stub
-
+        CodecheckerServerThread server = CodeCheckerContext.getInstance().getServerObject(project);
     }
 
     @Override
@@ -22,14 +25,12 @@ public class CodeCheckerNature implements IProjectNature {
 
     @Override
     public IProject getProject() {
-        // TODO Auto-generated method stub
-        return null;
+        return project;
     }
 
     @Override
     public void setProject(IProject project) {
-        // TODO Auto-generated method stub
-
+        this.project = project;
     }
 
 }
